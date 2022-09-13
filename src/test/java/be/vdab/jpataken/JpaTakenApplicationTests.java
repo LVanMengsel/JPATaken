d98576f6074a3292230d3936a3aad3b369475662
LVanMengsel <lorenz.vm@telenet.be>
2022-09-13 11:32:07 +0200

    Implementatie DataSourceTest

---
+++ src/test/java/be/vdab/jpataken/JpaTakenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.jpataken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTakenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
